41a9b0221c94fef20ec575c02d7ba001a10a2385
toshilk90 <138865266+toshilk90@users.noreply.github.com>
2024-04-03 13:19:32 +0530

    Add files via upload

---
+++ addToCart.js
@@ -0,0 +1,54 @@
+import { updateCartValue } from "../updateCartValue";
+import { getCartProductFromLS } from "./getCartProducts";
+import { showToast } from "./showToast";
+
+
+getCartProductFromLS();
+
+export const addToCart = (event, id, stock) => {
+    let arrLocalStorageProduct = getCartProductFromLS();
+  
+    const currentProdElem = document.querySelector(`#card${id}`);
+    let quantity = currentProdElem.querySelector(".productQuantity").innerText;
+    let price = currentProdElem.querySelector(".productPrice").innerText;
+    //   console.log(quantity, price);
+    price = price.replace("₹", "");
+  
+    let existingProd = arrLocalStorageProduct.find(
+      (curProd) => curProd.id === id
+    );
+  
+    console.log(existingProd);
+  
+    if (existingProd && quantity > 1) {
+      quantity = Number(existingProd.quantity) + Number(quantity);
+      price = Number(price * quantity);
+      let updatedCart = { id, quantity, price };
+  
+      updatedCart = arrLocalStorageProduct.map((curProd) => {
+        return curProd.id === id ? updatedCart : curProd;
+      });
+      console.log(updatedCart);
+  
+      
+    }
+  
+    if (existingProd) {
+      // alert("bhai duplicate hai");
+      return false;
+    }
+  
+  
+    price = Number(price * quantity);
+    quantity = Number(quantity);
+  
+    arrLocalStorageProduct.push({ id, quantity, price });
+    localStorage.setItem("cartProductLS", JSON.stringify(arrLocalStorageProduct));
+  
+    //update the cart button value
+    updateCartValue(arrLocalStorageProduct);
+
+    showToast("add", id);
+   
+  };
+  
